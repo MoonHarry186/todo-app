76fcfd5639bfeb87f3006edccf495d0898ff2104
Harry Moon <phucnh@quantsville.com>
2022-11-02 11:48:20 +0700

    fix long paragraph

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,70 +1,89 @@
 import { useState } from "react";
-import { FaWindowClose, FaEdit } from 'react-icons/fa'
+import { FaWindowClose, FaEdit } from "react-icons/fa";
 import TodoForm from "./TodoForm";
 
-function Todo({todoList, removeTodo, updateTodo, updateStatus}) {
-	
-	// State for updating action
-	const [edit, setEdit] = useState({   
-		id: null,
-		value: ''
-	})
+function Todo({ todoList, removeTodo, updateTodo, updateStatus }) {
+  // State for updating action
+  const [edit, setEdit] = useState({
+    id: null,
+    value: "",
+  });
 
-	// When submit updated form
-	const submitUpdate = value => {
-		const newValue = {
-			id: edit.id,
-			value: value,
-			status: edit.status
-		}
-		updateTodo(edit.id, newValue);
-		setEdit({
-			id: null,
-			value: '',
-			status: edit.status
-		})
-	}
+  // When submit updated form
+  const submitUpdate = (value) => {
+    const newValue = {
+      id: edit.id,
+      value: value,
+      status: edit.status,
+    };
+    updateTodo(edit.id, newValue);
+    setEdit({
+      id: null,
+      value: "",
+      status: edit.status,
+    });
+  };
 
-	// Click onto todo
-	const handleClick = (e, todo) => {
-		if (todo.status === "uncompleted") {
-			const newValue = {
-				id: todo.id,
-				value: todo.value,
-				status: 'completed'
-			}
-			// Mark the work as "completed" or "uncompleted"
-			updateStatus(todo.id, newValue)
-			e.target.classList.add('completed')
-		} else {
-			const newValue = {
-				id: todo.id,
-				value: todo.value,
-				status: 'uncompleted'
-			}
+  // Click onto todo
+  const handleClick = (e, todo) => {
+    if (todo.status === "uncompleted") {
+      const newValue = {
+        id: todo.id,
+        value: todo.value,
+        status: "completed",
+      };
+      // Mark the work as "completed" or "uncompleted"
+      updateStatus(todo.id, newValue);
+      e.target.classList.add("completed");
+    } else {
+      const newValue = {
+        id: todo.id,
+        value: todo.value,
+        status: "uncompleted",
+      };
 
-			// Mark the work as "completed" or "uncompleted"
-			updateStatus(todo.id, newValue)
-			e.target.classList.remove('completed')
+      // Mark the work as "completed" or "uncompleted"
+      updateStatus(todo.id, newValue);
+      e.target.classList.remove("completed");
+    }
+  };
 
-		}
-	}
-	
-	// Update Mode
-	if(edit.id) {
-		return <TodoForm editValue={edit.value} onSubmit={submitUpdate}/>
-	}
-	
-	// List works
-	return (todoList.map((todo, index) => (
-		<div key={todo.id} className={todo.status === "completed" ? "todo-box-child completed" : "todo-box-child"} onClick={(e) => handleClick(e, todo)}>
-			<h2>{todo.value}</h2>
-			<div className="icons">
-				<span className="delete" onClick={() => removeTodo(todo.id)}><FaWindowClose /></span>
-				<span className="edit" onClick={() => setEdit({id: todo.id, value: todo.value, status: todo.status})}><FaEdit /></span>
-			</div>
-		</div>
-	)));
+  // Update Mode
+  if (edit.id) {
+    return <TodoForm editValue={edit.value} onSubmit={submitUpdate} />;
+  }
+
+  // List works
+  return (
+    <div className="total-list">
+      {todoList.map((todo, index) => (
+        <div
+          key={todo.id}
+          className={
+            todo.status === "completed"
+              ? "todo-box-child completed"
+              : "todo-box-child"
+          }
+          onClick={(e) => handleClick(e, todo)}
+        >
+          <h2>{todo.value}</h2>
+          <div className="icons">
+            <span className="delete" onClick={() => removeTodo(todo.id)}>
+              <FaWindowClose />
+            </span>
+            <span
+              className="edit"
+              onClick={() =>
+                setEdit({ id: todo.id, value: todo.value, status: todo.status })
+              }
+            >
+              <FaEdit />
+            </span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Todo;
